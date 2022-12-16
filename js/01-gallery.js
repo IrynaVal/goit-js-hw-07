ed0d3fc0c9e799ca27961376731d7ce543469f4d
IrynaVal <piv8050@gmail.com>
2022-12-16 23:52:24 +0200

    hw-js-07

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -18,7 +18,6 @@ const galleryMarkup = galleryItems.map(({ preview, original, description }) =>
 </div>`);
 
 gallery.innerHTML = galleryMarkup.join('');
-// console.log(gallery);
 
 gallery.addEventListener('click', onGalleryImgClick);
 
@@ -27,79 +26,21 @@ function onGalleryImgClick(evt) {
         return;
     }
     evt.preventDefault();
-    // console.log(evt.target.dataset.source);
-    // console.log(evt.target)
-    // console.log(evt.target.src)
+    
     const originalImg = evt.target.dataset.source;
 
-    const instance = basicLightbox.create(`
-    <img src="${originalImg}">
-`
-        // , {
-        // onClose: (instance) => {
-        //     instance.element().querySelector('a').onclick = instance.close
-        // }
-        // }
-    )
-
-    window.addEventListener('keydown', onEscPress); 
-    instance.show();
-    // instance.close(onEscPress);
-}
+  const instance = basicLightbox.create(`<img src="${originalImg}">`, {
+    onShow: (instance) => {
+      window.addEventListener('keydown', onEscPress);
+    }
+  })
+  
+  instance.show();
 
 function onEscPress(evt) {
-    console.log(instance.close());
-    if (evt.code === 'Escape') {
-         window.removeEventListener('keydown', onEscPress);
-        basicLightbox.instance.close();
+    if(evt.code === 'Escape') {
+      instance.close();
+      window.removeEventListener('keydown', onEscPress);
     }
-      
-    // console.log(evt);
 }
-// const instance = basicLightbox.create(evt.target, {
-    //     onShow: (instance) => {
-    //     evt.target.src = evt.target.dataset.source;
-    // }});
-
-//  const instance = basicLightbox.create(`<img
-// //       class="gallery__image"
-// //       src="https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825_1280.jpg"
-// //           alt="img"
-// //     />
-	
-// // `)
-
-
-
-
-// instance.show()
-// instance.show(() => console.log('lightbox now visible'))
-// instance.close()
-// instance.close(() => console.log('lightbox not visible anymore'))
-
-
-/*
-	 * Function that gets executed before the lightbox will be shown.
-	 * Returning false will prevent the lightbox from showing.
-	 */
-	// onShow: (instance) => {},
-	/*
-	 * Function that gets executed before the lightbox closes.
-	 * Returning false will prevent the lightbox from closing.
-	 */
-	// onClose: (instance) => {}
-
-
-//     const instance = basicLightbox.create(`
-//     <div class="modal">
-//         <p>A custom modal that has been styled independently. It's not part of basicLightbox, but perfectly shows its flexibility.</p>
-//         <input placeholder="Type something">
-//         <a>Close</a>
-//     </div>
-// `, {
-//     onShow: (instance) => {
-//         instance.element().querySelector('a').onclick = instance.close
-//     }
-// })
-
-// instance.show()
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,8 +1,22 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
 
+const gallery = document.querySelector('.gallery');
 
+const galleryMarkup = galleryItems.map(({ preview, original, description }) => 
+   `<li><a class="gallery__item" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a></li>`);
 
-console.log(simpleLightbox);
+gallery.innerHTML = galleryMarkup.join('');
+// console.log(gallery);
+
+// console.log(SimpleLightbox);
+
+let lightboxGallery = new SimpleLightbox('.gallery a', {captionsData: 'alt', captionPosition: 'bottom',captionDelay: '250',});
